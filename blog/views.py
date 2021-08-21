1a96e535713f3e233da7687f2f2ce5e311eabed3
kwakeleven <87745990+kwakeleven@users.noreply.github.com>
2021-08-21 20:46:00 +0900

    update

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
+
+
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+
+
+class PostDetail(DetailView):
+    model = Post
 
 
 def index(request):
